c497375ab2933b1d7333aece975740a11d6bf83a
Val <vmiha@yahoo-inc.com>
2016-11-12 21:44:10 -0800

    menu

--- app/src/main/java/com/codepath/travellog/apps/activities/HomeActivity.java
+++ app/src/main/java/com/codepath/travellog/apps/activities/HomeActivity.java
@@ -1,16 +1,28 @@
 package com.codepath.travellog.apps.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.codepath.travellog.R;
 import com.codepath.travellog.apps.fragments.MapsFragment;
+import com.codepath.travellog.apps.fragments.ProfileFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private MapsFragment mapsFragment;
+
+    private DrawerLayout mDrawer;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +33,20 @@ public class HomeActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+
+        // set up the drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        setupDrawerContent(nvDrawer);
+
+        nvDrawer.getMenu().getItem(0).setChecked(true);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, new MapsFragment()).commit();
+        setTitle(R.string.app_name);
+
         /**
-         * create a hook to the fragment so that you can invoke methods on it from the activity
+         * created a hook to the fragment so that you can invoke methods on it from the activity
          * mapsFragment.setMarker("Set a marker");
          * make sure to call methods on mapsFragment only after it has loaded - onConnected() has been called so outside of this method
          */
@@ -31,4 +55,90 @@ public class HomeActivity extends AppCompatActivity {
         }
 
     }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+
+    }
+    public void selectDrawerItem(MenuItem menuItem) {
+
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.map:
+                fragmentClass = MapsFragment.class;
+                break;
+            case R.id.trips:
+                fragmentClass = MapsFragment.class;
+                break;
+
+            case R.id.profile:
+                fragmentClass = ProfileFragment.class;
+                break;
+            default:
+                fragmentClass = MapsFragment.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/codepath/travellog/apps/fragments/MapsFragment.java
+++ app/src/main/java/com/codepath/travellog/apps/fragments/MapsFragment.java
@@ -97,8 +97,6 @@ public class MapsFragment extends Fragment implements
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     /*
